b9a7d825e57b2b3c9de4b08a7034b333596b1af1
RehmatFalcon <niroulakushal31@gmail.com>
2020-04-17 16:11:39 +0545

    Feat: add bootstrap and theming

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <button class="btn btn-info" @click="toggleTheme">
+        Day/Night
+    </button>
+    <div id="nav" class="cst">
+      <router-link to="/" class="btn btn-primary">Home</router-link> |
+      <router-link to="/about" class="btn btn-warning">About</router-link>
     </div>
-    <router-view />
+    <transition name="slide-fade" mode="out-in">       
+      <router-view />
+    </transition>
   </div>
 </template>
 
+<script>
+import "./styles.scss";
+const bodyElm = document.querySelector("body");
+const tempTransitionalClass = "temp-transitional";
+
+export default {
+  data () {
+    return {
+      theme : "light"
+    }
+  },
+  methods: {
+    toggleTheme() {
+      bodyElm.classList.add(tempTransitionalClass);
+      if(this.theme === "light") {
+        this.theme = "dark";
+        bodyElm.classList.remove("theme-light");
+        bodyElm.classList.add("theme-dark");
+      }
+      else {
+        this.theme = "light";
+        bodyElm.classList.remove("theme-dark");
+        bodyElm.classList.add("theme-light");
+      }
+      setTimeout(() => bodyElm.classList.remove(tempTransitionalClass), 1000);
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+} 
+.slide-fade-enter {
+  transform : translate(-10%);
+}
+.slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10%);
+  opacity: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -22,11 +74,13 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+  }
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.temp-transitional {
+  &, & *, & *:before, & *:after {
+    transition: all .3s;
   }
 }
 </style>
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 offset-3">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Dark / Light mode</h3>
+          </div>
+          <div
+            class="card-body"
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta ea nostrum perferendis, numquam neque quos unde reprehenderit eum nam? Nihil odio enim libero vero officia, iste repudiandae at velit ea.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
