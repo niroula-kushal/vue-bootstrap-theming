ce92f1c028d6249d635c16a19ca48e5ad856d436
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 08:15:34 +0000

    Deploy RehmatFalcon/vue-bootstrap-theming to github.com/RehmatFalcon/vue-bootstrap-theming.git:gh-pages

---
+++ precache-manifest.d7949ff5f6bc08fad7083dc65b31ba61.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00c463d2b02a09e3067",
+    "url": "/vue-bootstrap-theming/css/app.4df532a0.css"
+  },
+  {
+    "revision": "76fcd3ae03d9ebd38f26",
+    "url": "/vue-bootstrap-theming/css/chunk-vendors.b36ebbc4.css"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/vue-bootstrap-theming/fonts/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/vue-bootstrap-theming/fonts/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/vue-bootstrap-theming/fonts/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/vue-bootstrap-theming/fonts/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/vue-bootstrap-theming/fonts/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/vue-bootstrap-theming/fonts/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/vue-bootstrap-theming/fonts/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/vue-bootstrap-theming/fonts/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/vue-bootstrap-theming/fonts/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/vue-bootstrap-theming/fonts/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/vue-bootstrap-theming/fonts/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/vue-bootstrap-theming/fonts/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/vue-bootstrap-theming/img/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/vue-bootstrap-theming/img/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/vue-bootstrap-theming/img/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "85d94e1db8bbeba9438043e94afbbb77",
+    "url": "/vue-bootstrap-theming/index.html"
+  },
+  {
+    "revision": "d00c463d2b02a09e3067",
+    "url": "/vue-bootstrap-theming/js/app.34470247.js"
+  },
+  {
+    "revision": "76fcd3ae03d9ebd38f26",
+    "url": "/vue-bootstrap-theming/js/chunk-vendors.8adcc538.js"
+  },
+  {
+    "revision": "33b83335ad1eb20058cf70981ee54010",
+    "url": "/vue-bootstrap-theming/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-bootstrap-theming/robots.txt"
+  }
+]);
